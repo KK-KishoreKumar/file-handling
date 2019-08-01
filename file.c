df98343f74cb68185bc80d0351de9769ccdd3114
taisoncar <carsontai0714@gmail.com>
2019-08-01 10:53:18 +0800

    creating functions for strings file handling

--- file.c
+++ file.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fcopy(char file1[],char file2[])
+void fcopys(char file1[],char file2[])
 {
    FILE *ptrf;
    ptrf=fopen(file1,"r");
@@ -16,13 +16,43 @@ void fcopy(char file1[],char file2[])
    fprintf(ptrf,"%s",c);
    fclose(ptrf);
 }
+
+void fwrites(char filename[],char text[])
+{
+    FILE *ptrf;
+    ptrf=fopen("test.txt","w");
+    fprintf(ptrf,"%s",text);
+    fclose(ptrf);
+}
+
+void freads(char filename[],char storing[])
+{
+    FILE *ptrf;
+    ptrf=fopen(filename,"r");
+    int i;
+    while((storing[i]=fgetc(ptrf))!=EOF)
+    {
+        ++i;
+    }
+    fclose(ptrf);
+}
+
+void fadds(char filename[],char text[])
+{
+    FILE *ptrf;
+    ptrf=fopen(filename,"a");
+    fprintf(ptrf,"%s",text);
+    fclose(ptrf);
+}
+
 int main()
 {
-    char filename1[100];
+    /* char filename1[100];
     char filename2[100];
     printf("file to copy from:\n");
     scanf("%s",filename1);
     printf("file to paste to:\n");
     scanf("%s",filename2);
-    fcopy(filename1,filename2);
+    fcopys(filename1,filename2);*/
+    fadds("test.txt","adding toppings?");
 }
